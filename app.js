c8c8e5b067434b304c85c52472dca3831f4ec8c5
gbs4ever <gbs4ever2@gmail.com>
2020-04-28 22:48:55 -0400

    added some missing package for post requests

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const parser = require("body-parser");
+app.use(parser.urlencoded({ extended: false }));
+app.use(parser.json());
+app.use(express.static("public"));
+
+//submit and make sure data has hit this route  and you should see the phone number
+app.post("/api", (req, res, next) => {
+  console.log(req.body);
+});
 
 app.listen(5000);
 console.log("server has loaded on port 5000");
